e34e6d8ae0df33998f973b65827e60fcf2219fb6
Patricia Ogura Ramos <patriciaoguraramos@gmail.com>
2022-03-28 11:27:06 -0300

    changed credit information

--- IOSProject/CreditViewController.swift
+++ IOSProject/CreditViewController.swift
@@ -9,43 +9,29 @@ import UIKit
 
 class CreditViewController: UIViewController {
     
-    
-    @IBOutlet weak var pNameLbl: UILabel!
-    @IBOutlet weak var pptsLbl: UILabel!
-    
     var pname: String?
     var ppts: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if pname != nil {
-            pNameLbl.text = pname
-        }
-        if ppts != nil {
-            pptsLbl.text = ppts
-        } else {
-            pptsLbl.text = "0"
-        }
-        
     }
     
     @IBAction func goBackBtn(_ sender: UIButton) {
         let controller = storyboard?.instantiateViewController(identifier: "GameViewController") as! GameViewController
-        controller.name = pNameLbl.text
-        controller.pontos = pptsLbl.text
+        
+        if pname != nil {
+            controller.name = pname
+        } else {
+            controller.name = ""
+        }
+        if ppts != nil {
+            controller.pontos = ppts
+        } else {
+            controller.pontos = "0"
+        }
+        
         controller.modalPresentationStyle = .fullScreen
         present(controller, animated: true, completion:nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
